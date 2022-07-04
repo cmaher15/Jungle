03986556b6c7787b03c264573056347549b249b0
cmaher15 <ceilidhemaher@hotmail.com>
2022-07-04 16:13:31 -0700

    create test for price validation

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -13,5 +13,11 @@ RSpec.describe Product, type: :model do
       expect(prod1).to be_invalid
       expect(prod1.errors.full_messages[0]).to eql("Name can't be blank")
     end
+    it "should be invalid without product price" do 
+      cat2 = Category.create(name: 'test2Cat')
+      prod2 = Product.create(name: 'monkeytree', quantity: 3, category: cat2)
+      expect(prod2).to be_invalid
+      expect(prod2.errors.full_messages[3]).to eql(nil)
+    end
   end
 end
