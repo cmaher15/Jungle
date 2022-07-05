ed61144191e892ce2cb5e25118799ce5ce7b5f1c
cmaher15 <ceilidhemaher@hotmail.com>
2022-07-04 17:50:08 -0700

    add case sensitivity to class method for user, write initial user tests

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,12 +5,12 @@ validates :password, presence: true
 validates :password, length: { minimum: 6 }
 validates :password_confirmation, presence: true
 validates :email, presence: true
-validates :email, confirmation: {case_sensitive: false}
 validates :name, presence: true
 validates :email, uniqueness: true
 
 def self.authenticate_with_credentials(email, password)
-  user = User.find_by_email(email)
+  newEmail = email.downcase
+  user = User.find_by_email(newEmail)
   if user && user.authenticate(password)
     return user
   else
--- spec/models/user_specs.rb
+++ spec/models/user_specs.rb
@@ -2,6 +2,14 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
   describe 'Validations' do
-    
+    it "should be valid with valid attributes" do
+      user1 = User.create(name: 'testPerson', email: 'test@123.com', password: 'testpassword', password_confirmation: 'testpassword')
+      expect(user1).to be_valid
+    end
+    it "should be invalid without a password" do
+      user2 = User.create(name: 'test2', email: 'test2@123.com', password_confirmation: '123456') 
+      expect(user2).to be_invalid
+      expect(user2.errors.full_messages[0]).to eql("Password can't be blank")
+    end
   end
 end
