de601298681832922f346f38d13b8735dcdac3fe
cmaher15 <ceilidhemaher@hotmail.com>
2022-07-04 18:05:01 -0700

    write more validation based tests for users

--- spec/models/user_specs.rb
+++ spec/models/user_specs.rb
@@ -21,5 +21,26 @@ RSpec.describe User, type: :model do
       expect(user4).to be_invalid
       expect(user4.errors.full_messages[0]).to eql("Password confirmation doesn't match Password")
     end
+    it "should be invalid if password does not meet minimum length" do
+      user5 = User.create(name: 'test5', email: 'test2@123.com', password: '123', password_confirmation: '123') 
+      expect(user5).to be_invalid
+      expect(user5.errors.full_messages[0]).to eql("Password is too short (minimum is 6 characters)")
+    end
+    it "should be invalid without a users name" do
+      user6 = User.create(email: 'test6@123.com', password: '123456', password_confirmation: '123456') 
+      expect(user6).to be_invalid
+      expect(user6.errors.full_messages[0]).to eql("Name can't be blank")
+    end
+    it "should be invalid without a users email" do
+      user7 = User.create(name: 'test7', password: '123456', password_confirmation: '123456') 
+      expect(user7).to be_invalid
+      expect(user7.errors.full_messages[0]).to eql("Email can't be blank")
+    end
+    it "should not allow a user to register with an email already in database" do
+      user8 = User.create(name: 'test8', email: 'test8@123.com', password: '123456', password_confirmation: '123456') 
+      user9 = User.create(name: 'test9', email: 'test8@123.com', password: '123456', password_confirmation: '123456')
+      expect(user9).to be_invalid
+      expect(user9.errors.full_messages[0]).to eql("Email has already been taken")
+    end
   end
 end
